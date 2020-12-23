99ce3865bc1a597962d8613919c32fc882bf9336
Jadtr <Jadtr@LAPTOP-9S4JHT3Q>
2020-12-23 21:47:19 +0100

    Student class

--- JavaProject/src/fr/epita/classes/Student.java
+++ JavaProject/src/fr/epita/classes/Student.java
@@ -1,5 +1,82 @@
 package fr.epita.classes;
 
-public class Student {
+import java.time.LocalDate;
 
+public class Student {
+	private int id;
+	private String first_name;
+	private String last_name;
+	private LocalDate dob;
+	private String gender;
+	
+	public Student() {
+		
+	}
+	
+	public Student(int id, String first_name, String last_name, LocalDate dob, String gender) {
+		super();
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.dob = dob;
+		this.gender = gender;
+	}
+	
+	public Student(String first_name, String last_name, LocalDate dob, String gender) {
+		super();
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.dob = dob;
+		this.gender = gender;
+	}
+
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", dob=" + dob
+				+ ", gender=" + gender + "]";
+	}
+	
+	
 }
