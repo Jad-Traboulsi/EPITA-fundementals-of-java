3f269fb0b36f889cecd4bf05f3cced1709843f69
Jadtr <Jadtr@LAPTOP-9S4JHT3Q>
2020-12-30 19:26:08 +0100

    Student Quiz DAO

--- JavaProject/src/fr/epita/doa/StudentQuizDAO.java
+++ JavaProject/src/fr/epita/doa/StudentQuizDAO.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
 import fr.epita.classes.FullQuestion;
 import fr.epita.classes.FullQuiz;
+import fr.epita.classes.Quiz;
 import fr.epita.classes.Student;
 
 public class StudentQuizDAO {
@@ -40,10 +42,9 @@ public class StudentQuizDAO {
 		}
 		return exists;
 	}
-	public void setGrade(Student student,FullQuiz fq) throws Exception{
+	public void calculateGrade(Student student,FullQuiz fq) throws Exception{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
-		QuestionsDOA questdao = new QuestionsDOA();
 		
 		int studentId = sdao.getStudentId(student) ;
 		int quizId = qdao.getQuizId(fq.getQuiz());
@@ -87,7 +88,6 @@ public class StudentQuizDAO {
 	public void updateGrade(Student student,FullQuiz fq) throws Exception{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
-		QuestionsDOA questdao = new QuestionsDOA();
 
 		int studentId = sdao.getStudentId(student) ;
 		int quizId = qdao.getQuizId(fq.getQuiz());
@@ -127,11 +127,39 @@ public class StudentQuizDAO {
 		}
 		
 	}
+	public void setGrade(Student student,FullQuiz fq,int grade) throws Exception{
+		StudentDAO sdao =  new StudentDAO();
+		QuizDAO qdao = new QuizDAO();
+
+		int studentId = sdao.getStudentId(student) ;
+		int quizId = qdao.getQuizId(fq.getQuiz());
+		
+		if(studentId== 0){
+			throw new Exception("Student doesnt exist");
+		}		
+		else if(quizId== 0){
+			throw new Exception("Quiz doesnt exist");
+		}else {
+			
+
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fundementals-of-java", "postgres", "");
+			
+			String inserting = "Insert public.\"Student_Quizes\" (grade,id_student,id_quiz) values (?,?,?);";
+			PreparedStatement insertingStatement = connection.prepareStatement(inserting);
+			insertingStatement.setInt(1, grade);
+			insertingStatement.setInt(2, studentId);
+			insertingStatement.setInt(3, quizId);
+			insertingStatement.execute();
+			connection.close();
+			
+			
+		}
+		
+	}
 
 	public int getGrade(Student student,FullQuiz fq) throws Exception{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
-		QuestionsDOA questdao = new QuestionsDOA();
 		int grade = 0;
 
 		int studentId = sdao.getStudentId(student) ;
@@ -162,5 +190,110 @@ public class StudentQuizDAO {
 		return grade;
 		
 	}
+	public Hashtable<Student,Integer> getQuizGrades(FullQuiz fq) throws Exception{
+		StudentDAO sdao =  new StudentDAO();
+		QuizDAO qdao = new QuizDAO();
+		
+		Hashtable<Student,Integer> out = new Hashtable<>();
+		int quizId = qdao.getQuizId(fq.getQuiz());
+		if(quizId== 0){
+			throw new Exception("Quiz doesnt exist");
+		}
+		else {
 
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fundementals-of-java", "postgres", "");
+			
+			String inserting = "select * from public.\"Student_Quizes\" where id_quiz = ?";
+			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
+			preparedStatement.setInt(1, quizId);
+			ResultSet rs = preparedStatement.executeQuery();
+			int grade = 0;
+			int studentId = 0;
+			while(rs.next()) {
+				grade = rs.getInt("grade");
+				studentId = rs.getInt("id_student");
+				Student s = sdao.getStudentById(studentId);
+				out.put(s,grade);
+				
+			}
+			connection.close();
+			
+			
+		}
+		return out;
+		
+	}
+	
+	
+	public ArrayList<FullQuiz> getStudentQuizesByGrade(Student student,int grade) throws Exception{
+		ArrayList<FullQuiz> out = new ArrayList<>();
+		
+		StudentDAO sdao =  new StudentDAO();
+		RelationQuizQuestionDAO rltnqqdao = new RelationQuizQuestionDAO();
+		QuizDAO quizdao = new QuizDAO();
+
+		int studentId = sdao.getStudentId(student) ;
+		int quizId = 0;
+		if(studentId== 0){
+			throw new Exception("Student doesnt exist");
+		}		
+		else {
+
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fundementals-of-java", "postgres", "");
+			
+			String inserting = "select id_quiz from public.\"Student_Quizes\" where grade = ? and id_student = ?;";
+			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
+			preparedStatement.setInt(1, grade);
+			preparedStatement.setInt(2, studentId);
+			ResultSet rs = preparedStatement.executeQuery();
+			while(rs.next()) {
+				quizId = rs.getInt("id_quiz");
+				Quiz quiz = new Quiz(quizId,quizdao.getTitleById(quizId));
+				out.add(rltnqqdao.getQuiz(quiz));
+				
+			}
+			connection.close();
+			
+			
+		}
+		
+		return out;
+	}
+	// get all quizes and grades of specific student
+	public Hashtable<FullQuiz, Integer> getAllStudentQuizes(Student student) throws Exception{
+		Hashtable<FullQuiz, Integer> out = new Hashtable<>();
+		
+		StudentDAO sdao =  new StudentDAO();
+		RelationQuizQuestionDAO rltnqqdao = new RelationQuizQuestionDAO();
+		QuizDAO quizdao = new QuizDAO();
+
+		int studentId = sdao.getStudentId(student) ;
+		int quizId = 0;
+		int grade = 0;
+		if(studentId== 0){
+			throw new Exception("Student doesnt exist");
+		}		
+		else {
+
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fundementals-of-java", "postgres", "");
+			
+			String inserting = "select * from public.\"Student_Quizes\" where and id_student = ?;";
+			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
+			preparedStatement.setInt(2, studentId);
+			ResultSet rs = preparedStatement.executeQuery();
+			while(rs.next()) {
+				quizId = rs.getInt("id_quiz");
+				grade = rs.getInt("grade");
+				Quiz quiz = new Quiz(quizId,quizdao.getTitleById(quizId));
+				out.put(rltnqqdao.getQuiz(quiz),grade);
+				
+			}
+			connection.close();
+			
+			
+		}
+		
+		return out;
+	}
+	
 }
