d0ce64a7c57079860e8135203e8936a76392fe78
Jadtr <Jadtr@LAPTOP-9S4JHT3Q>
2021-01-05 03:34:11 +0100

    Relation Student Quiz DAO

--- JavaProject/src/fr/epita/doa/StudentQuizDAO.java
+++ JavaProject/src/fr/epita/doa/StudentQuizDAO.java
@@ -1,9 +1,11 @@
 package fr.epita.doa;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -12,13 +14,14 @@ import fr.epita.classes.FullQuestion;
 import fr.epita.classes.FullQuiz;
 import fr.epita.classes.Quiz;
 import fr.epita.classes.Student;
+import fr.epita.services.filereader.Reader;
 
 public class StudentQuizDAO {
-	String database = "jdbc:postgresql://localhost:5432/fundementals-of-java";
-	String username = "postgres";
-	String password = "";
-	
-	public boolean isPresent(Student student,FullQuiz fq)throws Exception{
+	Reader r = new Reader();
+	String database = r.getDatabase();
+	String username = r.getUsername();
+	String password = r.getPassword();
+	public boolean isPresent(Student student,FullQuiz fq)throws IOException{
 		boolean exists = false;
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
@@ -26,26 +29,47 @@ public class StudentQuizDAO {
 		int quizId = qdao.getQuizId(fq.getQuiz());
 		
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else if(quizId== 0){
-			throw new Exception("Quiz doesnt exist");
+			throw new IOException("Quiz doesnt exist");
 		}
 		else {
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "select grade from public.\"Student_Quizes\" where id_quiz = ? and id_student = ?;";
-			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
-			preparedStatement.setInt(1, quizId);
-			preparedStatement.setInt(2, studentId);
-			ResultSet rs = preparedStatement.executeQuery();
-			while(rs.next()) {
-				exists = true;
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "select grade from public.\"Student_Quizes\" where id_quiz = ? and id_student = ?;";
+				preparedStatement = connection.prepareStatement(inserting);
+				preparedStatement.setInt(1, quizId);
+				preparedStatement.setInt(2, studentId);
+				ResultSet rs = preparedStatement.executeQuery();
+				while(rs.next()) {
+					exists = true;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(preparedStatement!=null) {
+					try {
+						preparedStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-			connection.close();
+			
 		}
 		return exists;
 	}
-	public void calculateGrade(Student student,FullQuiz fq) throws Exception{
+	public void calculateGrade(Student student,FullQuiz fq) throws IOException{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
 		
@@ -53,13 +77,14 @@ public class StudentQuizDAO {
 		int quizId = qdao.getQuizId(fq.getQuiz());
 		
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else if(quizId== 0){
-			throw new Exception("Quiz doesnt exist");
+			throw new IOException("Quiz doesnt exist");
 		}
 		// check if already placed
 		if(!isPresent(student,fq)) {
+			
 			StudentAnswersDAO studentAnswerdao = new StudentAnswersDAO();
 			Hashtable<FullQuestion,String> answers =  studentAnswerdao.getStudentAnswers(student,fq);
 			int studentGrade = 0;
@@ -73,21 +98,41 @@ public class StudentQuizDAO {
 					studentGrade+=key.getQuestion().getDifficulty();
 				}
 			}
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "INSERT INTO public.\"Student_Quizes\"(id_student, id_quiz, grade)VALUES (?,?,?);";
-			PreparedStatement insertingStatement = connection.prepareStatement(inserting);
-			insertingStatement.setInt(1, studentId);
-			insertingStatement.setInt(2, quizId);
-			insertingStatement.setInt(3, studentGrade);
-			insertingStatement.execute();
-			connection.close();
+			PreparedStatement insertingStatement = null;
+			Connection connection = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "INSERT INTO public.\"Student_Quizes\"(id_student, id_quiz, grade)VALUES (?,?,?);";
+				insertingStatement = connection.prepareStatement(inserting);
+				insertingStatement.setInt(1, studentId);
+				insertingStatement.setInt(2, quizId);
+				insertingStatement.setInt(3, studentGrade);
+				insertingStatement.execute();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(insertingStatement!=null) {
+					try {
+						insertingStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
 			
 			
 		}
 		
 	}
-	public void updateGrade(Student student,FullQuiz fq) throws Exception{
+	public void updateGrade(Student student,FullQuiz fq) throws IOException{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
 
@@ -95,10 +140,10 @@ public class StudentQuizDAO {
 		int quizId = qdao.getQuizId(fq.getQuiz());
 		
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else if(quizId== 0){
-			throw new Exception("Quiz doesnt exist");
+			throw new IOException("Quiz doesnt exist");
 		}else {
 			StudentAnswersDAO studentAnswerdao = new StudentAnswersDAO();
 			Hashtable<FullQuestion,String> answers =  studentAnswerdao.getStudentAnswers(student,fq);
@@ -114,21 +159,41 @@ public class StudentQuizDAO {
 					studentGrade+=key.getQuestion().getDifficulty();
 				}
 			}
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "update public.\"Student_Quizes\" set grade= ? where id_student = ? and id_quiz = ?;";
-			PreparedStatement insertingStatement = connection.prepareStatement(inserting);
-			insertingStatement.setInt(1, studentGrade);
-			insertingStatement.setInt(2, studentId);
-			insertingStatement.setInt(3, quizId);
-			insertingStatement.execute();
-			connection.close();
+			Connection connection = null;
+			PreparedStatement insertingStatement = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "update public.\"Student_Quizes\" set grade= ? where id_student = ? and id_quiz = ?;";
+				insertingStatement = connection.prepareStatement(inserting);
+				insertingStatement.setInt(1, studentGrade);
+				insertingStatement.setInt(2, studentId);
+				insertingStatement.setInt(3, quizId);
+				insertingStatement.execute();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(insertingStatement!=null) {
+					try {
+						insertingStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
 			
 			
 		}
 		
 	}
-	public void setGrade(Student student,FullQuiz fq,int grade) throws Exception{
+	public void setGrade(Student student,FullQuiz fq,int grade) throws IOException{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
 
@@ -136,28 +201,47 @@ public class StudentQuizDAO {
 		int quizId = qdao.getQuizId(fq.getQuiz());
 		
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else if(quizId== 0){
-			throw new Exception("Quiz doesnt exist");
+			throw new IOException("Quiz doesnt exist");
 		}else {
+			Connection connection = null;
+			PreparedStatement insertingStatement = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "Insert public.\"Student_Quizes\" (grade,id_student,id_quiz) values (?,?,?);";
+				insertingStatement = connection.prepareStatement(inserting);
+				insertingStatement.setInt(1, grade);
+				insertingStatement.setInt(2, studentId);
+				insertingStatement.setInt(3, quizId);
+				insertingStatement.execute();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(insertingStatement!=null) {
+					try {
+						insertingStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 			
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "Insert public.\"Student_Quizes\" (grade,id_student,id_quiz) values (?,?,?);";
-			PreparedStatement insertingStatement = connection.prepareStatement(inserting);
-			insertingStatement.setInt(1, grade);
-			insertingStatement.setInt(2, studentId);
-			insertingStatement.setInt(3, quizId);
-			insertingStatement.execute();
-			connection.close();
 			
 			
 		}
 		
 	}
 
-	public int getGrade(Student student,FullQuiz fq) throws Exception{
+	public int getGrade(Student student,FullQuiz fq) throws IOException{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
 		int grade = 0;
@@ -166,55 +250,96 @@ public class StudentQuizDAO {
 		int quizId = qdao.getQuizId(fq.getQuiz());
 		
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else if(quizId== 0){
-			throw new Exception("Quiz doesnt exist");
+			throw new IOException("Quiz doesnt exist");
 		}
 		else {
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "select grade from public.\"Student_Quizes\" where id_quiz = ? and id_student = ?;";
-			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
-			preparedStatement.setInt(1, quizId);
-			preparedStatement.setInt(2, studentId);
-			ResultSet rs = preparedStatement.executeQuery();
-			while(rs.next()) {
-				grade = rs.getInt("grade");
+			PreparedStatement preparedStatement = null;
+			Connection connection = null;	
+					
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "select grade from public.\"Student_Quizes\" where id_quiz = ? and id_student = ?;";
+				preparedStatement = connection.prepareStatement(inserting);
+				preparedStatement.setInt(1, quizId);
+				preparedStatement.setInt(2, studentId);
+				ResultSet rs = preparedStatement.executeQuery();
+				while(rs.next()) {
+					grade = rs.getInt("grade");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(preparedStatement!=null) {
+					try {
+						preparedStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-			connection.close();
+			
 			
 			
 		}
 		return grade;
 		
 	}
-	public Hashtable<Student,Integer> getQuizGrades(FullQuiz fq) throws Exception{
+	public Hashtable<Student,Integer> getQuizGrades(FullQuiz fq) throws IOException{
 		StudentDAO sdao =  new StudentDAO();
 		QuizDAO qdao = new QuizDAO();
 		
 		Hashtable<Student,Integer> out = new Hashtable<>();
 		int quizId = qdao.getQuizId(fq.getQuiz());
 		if(quizId== 0){
-			throw new Exception("Quiz doesnt exist");
+			throw new IOException("Quiz doesnt exist");
 		}
 		else {
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "select * from public.\"Student_Quizes\" where id_quiz = ?";
-			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
-			preparedStatement.setInt(1, quizId);
-			ResultSet rs = preparedStatement.executeQuery();
-			int grade = 0;
-			int studentId = 0;
-			while(rs.next()) {
-				grade = rs.getInt("grade");
-				studentId = rs.getInt("id_student");
-				Student s = sdao.getStudentById(studentId);
-				out.put(s,grade);
-				
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "select * from public.\"Student_Quizes\" where id_quiz = ?";
+				preparedStatement = connection.prepareStatement(inserting);
+				preparedStatement.setInt(1, quizId);
+				ResultSet rs = preparedStatement.executeQuery();
+				int grade = 0;
+				int studentId = 0;
+				while(rs.next()) {
+					grade = rs.getInt("grade");
+					studentId = rs.getInt("id_student");
+					Student s = sdao.getStudentById(studentId);
+					out.put(s,grade);
+					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(preparedStatement!=null) {
+					try {
+						preparedStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-			connection.close();
+			
 			
 			
 		}
@@ -223,7 +348,7 @@ public class StudentQuizDAO {
 	}
 	
 	
-	public ArrayList<FullQuiz> getStudentQuizesByGrade(Student student,int grade) throws Exception{
+	public ArrayList<FullQuiz> getStudentQuizesByGrade(Student student,int grade) throws IOException{
 		ArrayList<FullQuiz> out = new ArrayList<>();
 		
 		StudentDAO sdao =  new StudentDAO();
@@ -233,23 +358,43 @@ public class StudentQuizDAO {
 		int studentId = sdao.getStudentId(student) ;
 		int quizId = 0;
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else {
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "select id_quiz from public.\"Student_Quizes\" where grade = ? and id_student = ?;";
-			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
-			preparedStatement.setInt(1, grade);
-			preparedStatement.setInt(2, studentId);
-			ResultSet rs = preparedStatement.executeQuery();
-			while(rs.next()) {
-				quizId = rs.getInt("id_quiz");
-				Quiz quiz = new Quiz(quizId,quizdao.getTitleById(quizId));
-				out.add(rltnqqdao.getQuiz(quiz));
-				
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "select id_quiz from public.\"Student_Quizes\" where grade = ? and id_student = ?;";
+				preparedStatement = connection.prepareStatement(inserting);
+				preparedStatement.setInt(1, grade);
+				preparedStatement.setInt(2, studentId);
+				ResultSet rs = preparedStatement.executeQuery();
+				while(rs.next()) {
+					quizId = rs.getInt("id_quiz");
+					Quiz quiz = new Quiz(quizId,quizdao.getTitleById(quizId));
+					out.add(rltnqqdao.getQuiz(quiz));
+					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(preparedStatement!=null) {
+					try {
+						preparedStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-			connection.close();
+			
 			
 			
 		}
@@ -257,7 +402,7 @@ public class StudentQuizDAO {
 		return out;
 	}
 	// get all quizes and grades of specific student
-	public Hashtable<FullQuiz, Integer> getAllStudentQuizes(Student student) throws Exception{
+	public Hashtable<FullQuiz, Integer> getAllStudentQuizes(Student student) throws IOException{
 		Hashtable<FullQuiz, Integer> out = new Hashtable<>();
 		
 		StudentDAO sdao =  new StudentDAO();
@@ -268,24 +413,42 @@ public class StudentQuizDAO {
 		int quizId = 0;
 		int grade = 0;
 		if(studentId== 0){
-			throw new Exception("Student doesnt exist");
+			throw new IOException("Student doesnt exist");
 		}		
 		else {
-
-			Connection connection = DriverManager.getConnection(database, username, password);
-			String inserting = "select * from public.\"Student_Quizes\" where and id_student = ?;";
-			PreparedStatement preparedStatement = connection.prepareStatement(inserting);
-			preparedStatement.setInt(2, studentId);
-			ResultSet rs = preparedStatement.executeQuery();
-			while(rs.next()) {
-				quizId = rs.getInt("id_quiz");
-				grade = rs.getInt("grade");
-				Quiz quiz = new Quiz(quizId,quizdao.getTitleById(quizId));
-				out.put(rltnqqdao.getQuiz(quiz),grade);
-				
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			try {
+				connection = DriverManager.getConnection(database, username, password);
+				String inserting = "select * from public.\"Student_Quizes\" where and id_student = ?;";
+				preparedStatement = connection.prepareStatement(inserting);
+				preparedStatement.setInt(1, studentId);
+				ResultSet rs = preparedStatement.executeQuery();
+				while(rs.next()) {
+					quizId = rs.getInt("id_quiz");
+					grade = rs.getInt("grade");
+					Quiz quiz = new Quiz(quizId,quizdao.getTitleById(quizId));
+					out.put(rltnqqdao.getQuiz(quiz),grade);
+					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(preparedStatement!=null) {
+					try {
+						preparedStatement.close();
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 			}
-			connection.close();
-			
 			
 		}
 		
